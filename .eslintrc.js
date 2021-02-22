cd02ab357051341cf3aebddf42afb7c1a8a43f3c
Romain Clement <contact@romain-clement.net>
2021-02-22 10:07:25 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   ignorePatterns: ['**/sw.js'],
 }
